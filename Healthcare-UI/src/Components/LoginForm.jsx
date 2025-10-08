fce2c8c047365f6d748dea0c99e64037afd7bdd2
jagtapK <jagtapkajal54@gmail.com>
2025-10-08 17:29:12 +0530

    Updated fix position

--- Healthcare-UI/src/Components/LoginForm.jsx
+++ Healthcare-UI/src/Components/LoginForm.jsx
@@ -1,36 +1,49 @@
 import React from "react";
 import "./Login.css";
-import logo from "../assets/Images/logo.jpg";
+import logo from "../assets/Images/PMCLogo.webp";
 
 export default function LoginForm() {
   return (
-    <div className="login-page">
-      <div className="login-box">
-        <div className="login-logo">
-          <img src={logo} alt="Logo" />
-        </div>
-
-        <form className="login-form">
-          <div className="Welcome-Box">
-            {" "}
-            <h2>Welcome! </h2>
-            <p>Login By Entering the Information Below</p>{" "}
-          </div>
-
-          <input type="email" placeholder="Email" required />
-          <input type="password" placeholder="Password" required />
-          <div className="button-group">
-            <button className="B1" type="submit">
-              Login 
-            </button>
-        
-          </div>
-          
-          <p>
-            Don’t have an account? <a href="/register">Register</a>
-          </p>
-        </form>
+    <>
+      <div className="logo">
+        <img src={logo} alt="Logo" /> 
+        <h2>Pune Munciple Corporation</h2>
       </div>
-    </div>
+
+      <div className="login-page">
+            <div className="welcomebox">
+              <h2>Welcome !</h2>
+              <p>Login by entering the information below</p>
+            </div>
+        <div className="login-box">
+          <form className="login-form">
+
+            <div className="input-group">
+              <input type="email" id="email" required placeholder=" " />
+              <label htmlFor="email">Email / Mobile *</label>
+            </div>
+
+            <div className="input-group">
+              <input type="password" id="password" required placeholder=" " />
+              <label htmlFor="password">Password *</label>
+            </div>
+
+            <div className="forgot-password">
+              <a href="#">Forgot Password ?</a>
+            </div>
+
+            <button className="login-btn" type="submit">
+              Login
+            </button>
+
+            <div className="or-divider">OR</div>
+
+            <p className="register-text">
+              Don’t have an account? <a href="/register">Sign up here</a>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
