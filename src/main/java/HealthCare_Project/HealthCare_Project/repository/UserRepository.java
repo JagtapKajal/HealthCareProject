1e3f3e28e63b20df2bd476f76391ab6e10cf2862
jagtapK <jagtapkajal54@gmail.com>
2025-10-09 09:36:19 +0530

    Created repository

---
+++ src/main/java/HealthCare_Project/HealthCare_Project/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package HealthCare_Project.HealthCare_Project.repository;
+
+import HealthCare_Project.HealthCare_Project.entity.UserDetails;
+import org.apache.catalina.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserDetails, Integer> {
+    public UserDetails findByEmailAndPassword(String email, String password);
+}
